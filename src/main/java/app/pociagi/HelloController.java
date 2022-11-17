da87ac60c9609af92837707d5ce8e2460eee1d71
rafal <rafal5094@gmail.com>
2022-11-17 11:42:29 +0100

    Testing First Menu

--- src/main/java/app/pociagi/HelloController.java
+++ src/main/java/app/pociagi/HelloController.java
@@ -1,14 +1,10 @@
 package app.pociagi;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+//import javafx.scene.control.Label;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    protected void onHelloButtonClick() {}
 }
