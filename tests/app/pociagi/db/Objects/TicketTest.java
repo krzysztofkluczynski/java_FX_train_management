1eefc10a2bf2bfb218db066166aeea9ff41b8cdd
Krzysztof Kluczynski <krzysztof.klczynski22@gmail.com>
2023-01-17 23:25:59 +0100

    some testsv2

---
+++ tests/app/pociagi/db/Objects/TicketTest.java
@@ -0,0 +1,84 @@
+package app.pociagi.db.Objects;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TicketTest {
+
+    @Test
+    void pushToDB() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void setDiscountId() {
+    }
+
+    @Test
+    void setSurname() {
+    }
+
+    @Test
+    void setUserId() {
+    }
+
+    @Test
+    void getArrivalStationId() {
+    }
+
+    @Test
+    void getDepartureStationId() {
+    }
+
+    @Test
+    void getConnectionId() {
+    }
+
+    @Test
+    void getUserId() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getSurname() {
+    }
+
+    @Test
+    void getDate() {
+    }
+
+    @Test
+    void getCarId() {
+    }
+
+    @Test
+    void getDiscountId() {
+    }
+
+    @Test
+    void getSeatId() {
+    }
+
+    @Test
+    void setPrice() {
+    }
+
+    @Test
+    void getPrice() {
+    }
+
+    @Test
+    void setCarId() {
+    }
+
+    @Test
+    void setSeatId() {
+    }
+}
