82e20563a1067b019133c347ad931a1a23b5d628
jkowalsk <01169192@pw.edu.pl>
2023-01-30 10:10:04 +0100

    Added timestamp_diff.sql file

---
+++ functions/timestamp_diff.sql
@@ -0,0 +1,17 @@
+--------------------------------------------------------
+--  File created - poniedzia³ek-stycznia-30-2023   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Function TIMESTAMP_DIFF
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE FUNCTION "Z13"."TIMESTAMP_DIFF" (a TIMESTAMP, b TIMESTAMP)
+RETURN NUMBER
+AS
+BEGIN
+  RETURN EXTRACT (DAY    FROM (b-a))*24*60+
+         EXTRACT (HOUR   FROM (b-a))*60+
+         EXTRACT (MINUTE FROM (b-a));
+END;
+
+/
