6afe53ceb04fde29db05f5b8a88fb239fb9272f6
jkowalsk <01169192@pw.edu.pl>
2023-01-30 10:11:41 +0100

    Added tg_check_name_station.sql

---
+++ triggers/tg_check_name_station.sql
@@ -0,0 +1,21 @@
+--------------------------------------------------------
+--  File created - poniedzia³ek-stycznia-30-2023   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Trigger TG_CHECK_NAME_STATION
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE TRIGGER "Z13"."TG_CHECK_NAME_STATION" 
+BEFORE INSERT ON stations
+FOR EACH ROW
+DECLARE
+    stations_cnt    NUMBER;
+BEGIN
+    SELECT COUNT(*) INTO stations_cnt FROM stations WHERE name = :new.name;
+    IF stations_cnt > 0 THEN
+        raise_application_error(-20001, 'Station with this name exists already');
+    END IF;
+END;
+
+/
+ALTER TRIGGER "Z13"."TG_CHECK_NAME_STATION" ENABLE;
