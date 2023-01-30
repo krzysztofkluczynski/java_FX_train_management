2ba639a59fe62289aea626759ad4ab0faf815224
jkowalsk <01169192@pw.edu.pl>
2023-01-30 10:12:57 +0100

    Added tg_del_user_tickets.sql

---
+++ triggers/tg_del_user_tickets.sql
@@ -0,0 +1,16 @@
+--------------------------------------------------------
+--  File created - poniedzia³ek-stycznia-30-2023   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Trigger TG_DEL_USER_TICKETS
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE TRIGGER "Z13"."TG_DEL_USER_TICKETS" 
+BEFORE DELETE ON users
+FOR EACH ROW
+BEGIN
+    DELETE FROM tickets WHERE user_id = :old.user_id;
+END;
+
+/
+ALTER TRIGGER "Z13"."TG_DEL_USER_TICKETS" ENABLE;
