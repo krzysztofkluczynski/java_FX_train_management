1e48aaa2f835adf3ce629c7928cec6b8021ca8ba
jkowalsk <01169192@pw.edu.pl>
2023-01-30 10:12:11 +0100

    Added tg_check_stationo_in_stops.sql

---
+++ triggers/tg_check_station_in_stops.sql
@@ -0,0 +1,20 @@
+--------------------------------------------------------
+--  File created - poniedzia³ek-stycznia-30-2023   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Trigger TG_CHECK_STATION_IN_STOPS
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE TRIGGER "Z13"."TG_CHECK_STATION_IN_STOPS" 
+BEFORE DELETE ON stations
+FOR EACH ROW
+DECLARE
+    stops_cnt   NUMBER;
+BEGIN
+    SELECT COUNT(*) INTO stops_cnt FROM stops WHERE station_id = :old.station_id;
+    IF stops_cnt > 0 THEN
+        raise_application_error(-20001, 'Station cannot be deleted, there are stops on this station');
+    END IF;
+END;
+/
+ALTER TRIGGER "Z13"."TG_CHECK_STATION_IN_STOPS" ENABLE;
