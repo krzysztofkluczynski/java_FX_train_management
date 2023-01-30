1cd7af6f5a645a5220cbd4e63ee367b706d6d372
jkowalsk <01169192@pw.edu.pl>
2023-01-30 10:12:33 +0100

    Added tg_del_stops_and_tickets.sql

---
+++ triggers/tg_del_stops_and_tickets.sql
@@ -0,0 +1,17 @@
+--------------------------------------------------------
+--  File created - poniedzia³ek-stycznia-30-2023   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Trigger TG_DEL_STOPS_AND_TICKETS
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE TRIGGER "Z13"."TG_DEL_STOPS_AND_TICKETS" 
+BEFORE DELETE ON connections
+FOR EACH ROW
+BEGIN
+    DELETE FROM stops WHERE connection_id = :old.connection_id;
+    DELETE FROM tickets WHERE connection_id = :old.connection_id;
+END;
+
+/
+ALTER TRIGGER "Z13"."TG_DEL_STOPS_AND_TICKETS" ENABLE;
